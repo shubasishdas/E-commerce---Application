7de81c69a7eb94857db4cdb25fe100f44ba2d4f2
shubasish <shubasish.me@gmail.com>
2022-07-01 10:08:13 +0600

    show data from api call

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,34 +1,31 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Mrs", lastName: "minju" },
-      age: 2,
+      monsters: [],
     };
+    console.log(1);
+  }
+
+  componentDidMount() {
+    console.log(3);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
   render() {
+    console.log(2);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}{" "}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(() => ({
-                name: { firstName: "Mrs", lastName: "yoongi" },
-              }));
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        ))}
       </div>
     );
   }
