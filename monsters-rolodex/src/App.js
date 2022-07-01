4c333a4b63adcae6500e265dd9925a20b8afaaf0
shubasish <shubasish.me@gmail.com>
2022-07-01 20:17:09 +0600

    undertstand performance issue for anonymous function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
     console.log(1);
   }
@@ -17,11 +18,26 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value.toLowerCase() });
+  };
+
   render() {
     console.log(2);
+    const { onSearchChange } = this;
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField)
+    );
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
+        <input
+          className="search-box"
+          placeholder="search monsters"
+          type="search"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
           </div>
