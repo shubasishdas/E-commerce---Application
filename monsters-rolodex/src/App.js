31adc627cf5fff121f860f635ae2e4427197dc1f
shubasish <shubasish.me@gmail.com>
2022-07-01 06:50:57 +0600

    Added state for testing dynamic value

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,23 +3,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "minju",
+    };
+  }
+
   render() {
-    console.log({ this: this });
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>Hi {this.state.name}</p>
+          <button
+            onClick={() => {
+              this.setState({ name: "yoongi" });
+            }}
           >
-            Learn React
-          </a>
+            Change Name
+          </button>
         </header>
       </div>
     );
