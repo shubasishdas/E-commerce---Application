d3348029fbef6a5b710871cc0273bfab3e884603
shubasish <shubasish.me@gmail.com>
2022-07-07 19:56:20 +0600

    Set routing and nested routing(outlet component)

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,35 +1,18 @@
-import CategoryList from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return <h1>This one belongs to Shop</h1>;
+};
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <CategoryList categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />}>
+        <Route path="" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
