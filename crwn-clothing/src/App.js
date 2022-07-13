2f3115a6d0524e8d14bb3a6d4d1734220aa4193b
shubasish <shubasish.me@gmail.com>
2022-07-13 23:58:55 +0600

    Set authentication

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import NavigationBar from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return <h1>This one belongs to Shop</h1>;
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<NavigationBar />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
